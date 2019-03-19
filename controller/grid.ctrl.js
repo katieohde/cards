57d0754ab93a7e5874ca0420942fefa9d910ac5f
Kathleen Ohde <kathleenohde@Kathleens-MacBook-Pro.local>
2019-03-19 18:02:55 -0500

    create version

--- controller/grid.ctrl.js
+++ controller/grid.ctrl.js
@@ -1,3 +1,9 @@
 angular.module('app.core').controller('gridController', function() {
     var vm = this;
+
+    var max = 30;
+    var min = 1;
+
+    var damage = Math.max(Math.floor(Math.random() * max) + 1, min);
+    console.log(damage);
 });
